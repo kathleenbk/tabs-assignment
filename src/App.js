03cd5bd2f7371f01507afcce42982458365544b6
kathleenbk <katie.kava3@gmail.com>
2022-05-09 11:17:20 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import Tabs from './components/tabs'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main-container'>
+        <Tabs tabsArray={[
+          {tabName:"Tab 1", tabContent:"Tab 1 content is showing here."}, {tabName:"Tab 2", tabContent:"Tab 2 content is showing here."}, {tabName:"Tab 3", tabContent:"Tab 3 content is showing here."}]}/>
+      </div>
     </div>
   );
 }
